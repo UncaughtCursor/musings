f758c439788d02b130afec18b7fea4f93c353f8b
UncaughtCursor <UncaughtCursor@gmail.com>
2024-01-08 09:57:55 -0500

    Quartz sync: Jan 8, 2024, 9:57 AM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -6,7 +6,10 @@ function PageTitle({ fileData, cfg, displayClass }: QuartzComponentProps) {
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h1 class={`page-title ${displayClass ?? ""}`}>
-      <a href={baseDir}>{title}</a>
+      <a href={baseDir}>
+		<img src="/static/icon.png" />
+		{title}
+	  </a>
     </h1>
   )
 }
@@ -14,6 +17,11 @@ function PageTitle({ fileData, cfg, displayClass }: QuartzComponentProps) {
 PageTitle.css = `
 .page-title {
   margin: 0;
+}
+.page-title > a > img {
+  height: 1.75rem;
+  margin: 0;
+  margin-right: 0.5rem;
 }
 `
 
